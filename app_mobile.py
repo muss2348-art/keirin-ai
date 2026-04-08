5fcdeab7fb3f4f91496be567b10b29054d824f17
muss2348-art <muss2348@gmail.com>
2026-04-08 22:56:15 +0900

    Update app_mobile.py

--- app_mobile.py
+++ app_mobile.py
@@ -1,4 +1,6 @@
+import os
 import streamlit as st
 
-st.title("新しいモバイル版です")
-st.write("WINTICKETなし確認用")
+st.title("確認用アプリ")
+st.write("これは新しい app_mobile.py です")
+st.write("実行ファイル:", os.path.abspath(__file__))
