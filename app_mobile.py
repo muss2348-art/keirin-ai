7fb4b510f7f11024df43c1d9a3cc3d6efd88b905
muss2348-art <muss2348@gmail.com>
2026-04-08 23:05:03 +0900

    Update app_mobile.py

--- app_mobile.py
+++ app_mobile.py
@@ -1,3 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="MOBILE TEST", layout="centered")
+st.title("MOBILE TEST 12345")
+st.write("これは app_mobile.py です")
+st.stop()
+
+
+
 import os
 import streamlit as st
 
